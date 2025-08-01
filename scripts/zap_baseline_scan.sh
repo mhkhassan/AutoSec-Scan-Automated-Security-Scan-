6101a804df59d57f5e036a54937a2917f8e39554
mhkhassan <hassanofficial9703@gmail.com>
2025-08-01 17:10:52 +0500

    Modified the docker image

--- scripts/zap_baseline_scan.sh
+++ scripts/zap_baseline_scan.sh
@@ -4,8 +4,8 @@
 TARGET_URL="$1"
 REPORT_FILE="zap_report.html"
 
-# Run ZAP Baseline Scan using DockerHub image
-docker run --rm -v "$(pwd)":/zap/wrk/:rw -t owasp/zap2docker-stable \
+# Run ZAP Baseline Scan using GHCR image
+docker run --rm -v "$(pwd)":/zap/wrk/:rw -t ghcr.io/zaproxy/zaproxy:stable \
     zap-baseline.py -t "$TARGET_URL" -g gen.conf -r "$REPORT_FILE" || true
 
 # Check if the report was generated
